3cabc4330a1c867d7b06c9466ec32569c37d692d
Creative-Adarsh <professional.adarsh.pro@gmail.com>
2025-09-15 23:07:51 +0530

    Add files via upload

---
+++ largest of two number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+   int a,b;
+   printf("Enter two numbers:-\n");
+   scanf("%d ",&a,&b);
+   if (a>b)
+   {
+    printf("%d is greatest",a);
+   }
+   else
+   {
+    printf("%d is greatest",b);
+   }
+   
+    
+    getch();
+}
