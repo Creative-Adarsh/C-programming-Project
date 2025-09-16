a8384e0539eea5485a7fcbbb235d776b38fd1ac3
Creative-Adarsh <professional.adarsh.pro@gmail.com>
2025-09-17 00:18:04 +0530

    Add files via upload

---
+++ Uppercase , lowercase, number and special character.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main(){
+    char letter;
+    printf("Enter any thing:");
+    scanf("%c",&letter);
+
+    if (letter >='a' && letter <='z')
+    {
+        printf("%c is a lowercase ",letter);
+    }
+    else if (letter >='A' && letter <='Z')
+    {
+        printf("%c is a uppercase ",letter);
+    }
+    else if (isdigit(letter))
+    {
+        printf("%c is a number",letter);
+    }
+    else
+    {
+        printf("%c is a special character",letter);
+    }
+    
+    
+    getch();
+}
