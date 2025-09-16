76dc1cccb9b9ec0508d6c8075823a091e657514d
Creative-Adarsh <professional.adarsh.pro@gmail.com>
2025-09-17 00:47:57 +0530

    Add files via upload

---
+++ prime number or not.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main(){
+    int num, i, isPrime=1;
+    printf("Enter a positive number : ");
+    scanf("%d",&num);
+
+    if (num <=1)
+    {
+        isPrime =0;
+    }
+    else
+    {
+        for (i=2 ; i <=num /2; i++)
+        {
+            if (num % i==0)
+            {
+                isPrime =0;
+                break;
+            }
+            
+        }
+        
+    }
+    if (isPrime)
+    {
+        printf("%d is a prome number", num);
+    }
+    else
+    {
+        printf("%d is not a prime number",num);
+    }
+    
+    
+    getch();
+}
