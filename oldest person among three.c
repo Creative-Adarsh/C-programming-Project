475fb17f2e127db9ec6a69a7d38ad0f90f38d46f
Creative-Adarsh <professional.adarsh.pro@gmail.com>
2025-09-17 13:24:14 +0530

    Add files via upload

---
+++ oldest person among three.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main(){
+    int age_1, age_2, age_3;
+    printf("Enter the age of the first person: ");
+    scanf("%d",&age_1);
+    printf("Enter the age of the second person: ");
+    scanf("%d",&age_2);
+    printf("Enter tthe age of the third person: ");
+    scanf("%d",&age_3);
+
+    if (age_1 > age_2 && age_1 > age_3)
+    {
+        printf("The person of %d years is oldest",age_1);
+    }
+    else if (age_2 > age_1 && age_2 > age_3)
+    {
+        printf("The person of %d years is oldest",age_2);
+    }
+    else
+    {
+        printf("The person of %d years is oldest",age_3);
+    }
+    
+    
+    
+
+
+    getch();
+}
